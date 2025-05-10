a53c64c93054527b29785e723ee37567c6b8642c
Tiago Alves <tiagonewalves@hotmail.com>
2025-05-09 22:04:46 -0300

    feat: add prisma and initial schema setup

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,6 @@ app.use(cors())
 app.use("/api", router)
 app.use(errorHandlerMiddleware)
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`))
