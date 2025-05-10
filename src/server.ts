694665d72e23c456b7d9f2021cdf992e2fdd03f8
Tiago Alves <tiagonewalves@hotmail.com>
2025-05-09 21:15:30 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import cors from 'cors'
+import express from 'express'
+import { router } from './router'
+import { errorHandlerMiddleware } from './middlewares/error-handler'
+
+const app = express()
+
+app.use(cors())
+app.use("/api", router)
+app.use(errorHandlerMiddleware)
+
+const PORT = 3000
+
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`))
