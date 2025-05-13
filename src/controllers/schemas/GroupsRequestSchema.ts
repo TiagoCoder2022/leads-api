bd60c5e60906e295fe40adb133b8552f8a417451
Tiago Alves <tiagonewalves@hotmail.com>
2025-05-13 13:38:31 -0300

    feat: add groups route to get, update and delete

---
+++ src/controllers/schemas/GroupsRequestSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const CreateGroupRequestSchema = z.object({
+  name: z.string().min(2),
+  description: z.string(),
+})
+
+export const UpdateGroupRequestSchema = z.object({
+  name: z.string().min(2).optional(),
+  description: z.string().optional(),
+})
--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,24 @@
 import { Router } from "express";
 import { HttpError } from "./erros/HttpError";
 import { LeadsController } from "./controllers/LeadsController";
+import { GroupsController } from "./controllers/GroupsController";
 
-const router = Router()
+const router = Router();
 
 const leadsController = new LeadsController();
+const groupsController = new GroupsController();
 
 router.get("/leads", leadsController.index);
 router.post("/leads", leadsController.create);
 router.get("/leads/:id", leadsController.show);
 router.put("/leads/:id", leadsController.update);
 router.delete("/leads/:id", leadsController.show);
+
+router.get("/groups", groupsController.index);
+router.post("/groups", groupsController.create);
+router.get("/groups/:id", groupsController.show);
+router.put("/groups/:id", groupsController.update);
+router.delete("/groups/:id", groupsController.delete);
 
 router.get("/test", async (req, res, next) => {
  try {
