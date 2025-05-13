5fe9dcc6ba6b089afefcc5e13ad9ba39a6e1b0c1
Tiago Alves <tiagonewalves@hotmail.com>
2025-05-13 14:24:23 -0300

    feat: add route to update a campaign

--- src/controllers/CampaignsController.ts
+++ src/controllers/CampaignsController.ts
@@ -1,6 +1,9 @@
 import { Handler } from "express";
 import { prisma } from "../database";
-import { CreateCampaignsRequestSchema } from "./schemas/CampaignsRequestSchema";
+import {
+  CreateCampaignsRequestSchema,
+  UpdateCampaignsRequestSchema,
+} from "./schemas/CampaignsRequestSchema";
 import { HttpError } from "../erros/HttpError";
 
 export class CampaignsController {
@@ -39,6 +42,28 @@ export class CampaignsController {
       if (!campaign) throw new HttpError(404, "Campaign not found");
 
       res.json(campaign);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  update: Handler = async (req, res, next) => {
+    try {
+      const id = Number(req.params.id);
+      const body = UpdateCampaignsRequestSchema.parse(req.body);
+
+      const campaignExists = await prisma.campaign.findUnique({
+        where: { id },
+      });
+
+      if (!campaignExists) throw new HttpError(404, "Campaign not fout");
+
+      const updatedCampaign = await prisma.campaign.update({
+        data: body,
+        where: { id },
+      });
+
+      res.json(updatedCampaign);
     } catch (error) {
       next(error);
     }
