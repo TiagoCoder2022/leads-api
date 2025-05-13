e09675da3022149b9765c3382efb53102ca01370
Tiago Alves <tiagonewalves@hotmail.com>
2025-05-13 12:35:36 -0300

    feat: ensure leads can be searched by query params

--- src/controllers/schemas/LeadsRequestSchema.ts
+++ src/controllers/schemas/LeadsRequestSchema.ts
@@ -1,31 +1,33 @@
 import { z } from "zod";
 
-export const CreateLeadRequestSchema =  z.object({
+const LeadsStatusEnum = z.enum([
+  "New",
+  "Contacted",
+  "Qualified",
+  "Converted",
+  "Disqualified",
+  "Archived",
+]);
+
+export const GetLeadsRequestSchema = z.object({
+  page: z.string().optional(),
+  pageSize: z.string().optional(),
+  name: z.string().optional(),
+  status: LeadsStatusEnum.optional(),
+  sortBy: z.enum(["name", "status", "createdAt"]).optional(),
+  order: z.enum(["asc", "desc"]).optional(),
+});
+
+export const CreateLeadRequestSchema = z.object({
   name: z.string().min(2),
   email: z.string().email(),
   phone: z.string(),
-  status: z.enum([
-    "New",
-    "Contacted",
-    "Qualified",
-    "Converted",
-    "Disqualified",
-    "Archived",
-  ]).optional()
-})
+  status: LeadsStatusEnum.optional(),
+});
 
 export const UpdateLeadRequestSchema = z.object({
   name: z.string().min(2).optional(),
   email: z.string().email().optional(),
   phone: z.string().optional(),
-  status: z
-    .enum([
-      "New",
-      "Contacted",
-      "Qualified",
-      "Converted",
-      "Disqualified",
-      "Archived",
-    ])
-    .optional(),
+  status: LeadsStatusEnum.optional(),
 });
